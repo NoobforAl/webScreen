e3efc5160750384dc139d85581d612caf0d8b6ec
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-07-04 15:33:56 +0330

    fix main file with new change
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,12 @@
-use clap::Parser;
 use std::error::Error;
 
 #[path = "core/webscreen.rs"]
 mod web_screen;
 
-#[path = "logger/logger.rs"]
-mod logger;
-
 #[path = "cmd/cmd.rs"]
 mod cmd;
 
-fn make_file_name(format: &str) -> String {
-    return format!("./screenShot.{format}");
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
-    let args = cmd::Cli::parse();
-
-    // setup logger
-    logger::init_logger(args.debug);
-
-    let res = web_screen::web_screen(&args.url, args.timeout, &args.format, args.quality)?;
-    web_screen::save_img(make_file_name(&args.format).as_str(), res)?;
+    cmd::cmd()?;
     Ok(())
 }
